f70ab5f196a0e8d7bb89e734233cd588d0916ec7
Scott <oceass@gmail.com>
2024-09-03 08:16:44 -0400

    VOTD format.

--- app/Http/Controllers/API/VODController.php
+++ app/Http/Controllers/API/VODController.php
@@ -4,21 +4,14 @@ namespace App\Http\Controllers\API;
 
 use App\Models\VOTD;
 use Illuminate\Routing\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Validator; // Import the Validator facade
-use App\Models\Newsletter;
-use Illuminate\Support\Facades\Http;
-
 
 class VODController extends Controller
 {
 
-    public function index() {
+    public function index()
+    {
 
         $verse = VOTD::getVerseOfTheDay();
-
-        return $verse;
 
         if (!$verse) {
             return response()->json([
