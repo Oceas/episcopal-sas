94d45dbf57637d0b54f2fb776379eff7ea98b578
Scott <oceass@gmail.com>
2024-09-25 20:31:40 -0400

    Scrub navigation.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="flex space-x-4">
-                <div class="flex-1 bg-gray-200 p-4">
+                <div class="flex-1  p-4">
                     <livewire:recent-prayer-requests />
                 </div>
                 <div class="flex-1 bg-gray-200 p-4">Column 2</div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,7 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
+
                     <x-nav-link :href="route('prayers')" :active="request()->routeIs('prayers')">
                         {{ __('Prayers') }}
                     </x-nav-link>
