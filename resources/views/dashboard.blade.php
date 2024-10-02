ad970f1911fd97e8650e9dbca34e54424c69cae3
Scott <oceass@gmail.com>
2024-10-02 10:07:58 -0400

    Responsive stuff.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,12 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex space-x-4">
-                <div class="flex-1  p-4">
-                    <livewire:recent-prayer-requests />
-                </div>
-                <div class="flex-1 p-4">
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <!-- Core stats on the left in desktop/tablet, first on mobile -->
+                <div class="p-4">
                     <livewire:core-stats />
+                </div>
+                <!-- Recent prayer requests on the right in desktop/tablet, second on mobile -->
+                <div class="p-4">
+                    <livewire:recent-prayer-requests />
                 </div>
             </div>
         </div>
--- resources/views/devices.blade.php
+++ resources/views/devices.blade.php
@@ -8,30 +8,32 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <table class="table-auto w-full bg-white shadow-md rounded-lg">
-                        <thead class="bg-gray-200">
-                        <tr class="text-left">
-                            <th class="px-4 py-2 text-left">VID</th>
-                            <th class="px-4 py-2 text-left">Brand</th>
-                            <th class="px-4 py-2 text-left">Year</th>
-                            <th class="px-4 py-2 text-left">App Version</th>
-                            <th class="px-4 py-2 text-left">Push Token</th>
-                            <th class="px-4 py-2 text-left">Registered</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($devices as $device)
-                            <tr class="border-t">
-                                <td class="px-4 py-2">{{ $device->vid }}</td>
-                                <td class="px-4 py-2">{{ $device->brand }}</td>
-                                <td class="px-4 py-2">{{ $device->device_year_class }}</td>
-                                <td class="px-4 py-2">{{ $device->app_version }}</td>
-                                <td class="px-4 py-2">{{ $device->push_token }}</td>
-                                <td class="px-4 py-2">{{ $device->created_at->format('M d, Y') }}</td>
+                    <div class="overflow-x-auto">
+                        <table class="table-auto w-full bg-white shadow-md rounded-lg">
+                            <thead class="bg-gray-200">
+                            <tr class="text-left">
+                                <th class="px-4 py-2 text-left">VID</th>
+                                <th class="px-4 py-2 text-left">Brand</th>
+                                <th class="px-4 py-2 text-left">Year</th>
+                                <th class="px-4 py-2 text-left">App Version</th>
+                                <th class="px-4 py-2 text-left">Push Token</th>
+                                <th class="px-4 py-2 text-left">Registered</th>
                             </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                            @foreach ($devices as $device)
+                                <tr class="border-t">
+                                    <td class="px-4 py-2">{{ $device->vid }}</td>
+                                    <td class="px-4 py-2">{{ $device->brand }}</td>
+                                    <td class="px-4 py-2">{{ $device->device_year_class }}</td>
+                                    <td class="px-4 py-2">{{ $device->app_version }}</td>
+                                    <td class="px-4 py-2">{{ $device->push_token }}</td>
+                                    <td class="px-4 py-2">{{ $device->created_at->format('M d, Y') }}</td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                     <!-- Pagination -->
                     <div class="mt-4">
                         {{ $devices->links() }}
